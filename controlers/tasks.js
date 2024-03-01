56ce31d1ed50ce483ed83c347463953412a559e8
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-03-01 12:51:05 +0200

    update updateTask Controler and render the frontend on the home directory

--- controlers/tasks.js
+++ controlers/tasks.js
@@ -42,8 +42,21 @@ const deleteTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send('update task');
+const updateTask = async (req, res) => {
+  // res.status(200).json({id: taskID, data: req.body  });
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      newValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 module.exports = {
   getAllTasks,
