757e2ba62fa2857b7a4fc26c3c4a89c74d9ea36e
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-03-01 12:04:39 +0200

    Update getTask and delete task Controllers

--- controlers/tasks.js
+++ controlers/tasks.js
@@ -17,16 +17,34 @@ const createTask = async (req, res) => {
     res.status(500).json({ msg: err });
   }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task found with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
 const updateTask = (req, res) => {
   res.send('update task');
 };
-const deleteTask = (req, res) => {
-  res.send('delete task');
-};
-
 module.exports = {
   getAllTasks,
   createTask,
