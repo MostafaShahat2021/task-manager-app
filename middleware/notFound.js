a8b01a51be3b097d6c002e16f9264fcaeef5a7ff
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-03-03 18:44:42 +0200

    remove comment

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,3 +1,3 @@
-const notFound = (req, res) => res.status(404).send('Route dos not exist !');
+const notFound = (req, res) => res.status(404).send('Route dos not exist');
 
 module.exports = notFound;
